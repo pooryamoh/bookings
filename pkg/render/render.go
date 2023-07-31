6c49ec2d2f6b294f547362906de1527a25f11902
Poorya Mohammadi <pm1375pm@gmail.com>
2023-07-31 16:23:51 +0330

    Initiate project

---
+++ pkg/render/render.go
@@ -0,0 +1,86 @@
+package render
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"path/filepath"
+	"text/template"
+
+	"github.com/pooryamoh/bookings/pkg/config"
+	"github.com/pooryamoh/bookings/pkg/models"
+)
+var app *config.AppConfig
+
+func NewTemplates(a *config.AppConfig){
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData{
+	return td
+}
+
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+	if app.UseCache{
+		tc = app.TemplateCache
+	}else{
+		tc,_ = CreateTemplateCache();
+	}
+	
+	t, ok := tc[tmpl]
+
+	if !ok {
+		log.Fatal("can't find templae")
+	}
+
+	buf := new(bytes.Buffer)
+	td = AddDefaultData(td)
+	err := t.Execute(buf, td)
+
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	_,err = buf.WriteTo(w)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+}
+
+func CreateTemplateCache()(map[string] *template.Template,error){
+	myCache := map[string] *template.Template{}
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		return myCache, err
+	}
+
+	for _,page := range pages{
+		name := filepath.Base(page)
+		ts,err := template.New(name).ParseFiles(page)
+		if err != nil {
+			log.Println(err.Error())
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			log.Println(err.Error())
+			return myCache, err
+		}
+
+		if len(matches) > 0{
+			ts,err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				log.Println(err.Error())
+				return myCache, err
+			}
+		}
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+}
